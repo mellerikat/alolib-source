2fb49642184bcd5ec4d7784b0ab3fd723d35bb3b
ws.jang <ws.jang@lge.com>
2024-05-17 13:23:13 +0900

    hotfix: utils.py to Eng

--- alolib/utils.py
+++ alolib/utils.py
@@ -1,26 +1,19 @@
 # -*- coding: utf-8 -*-
-import os
-import shutil
-import pickle
 import json 
+import os
+import pickle
+import shutil
 
 def load_file(_data_file):
-    """ Description
-        -----------
-            - 파일을 로하하여 데이터로 가져온다.
-        Parameters
-        -----------
-            - data_file (str) : 로드할 데이터의 파일이름 (경로 포함)
-                                (확장자 지원 : csv, h5, tfrecord, pkl, json, params, log)
-            - option
-                - _print (bool) : 데이터 저장여부 출력
-        Return
-        -----------
-            - data (csv) : dataframe
-                    (pkl, json, params, log) : dictionary
-        Example
-        -----------
-            - data = load_file(data_file)
+    """ load file and return data
+        extension supported: (pkl, json, params, log)
+
+    Args:
+        data_file (str) : data file path
+                         
+    Returns: 
+        _data   (dict): loaded data
+
     """
     _data = None
     if _data_file != None:
@@ -46,26 +39,18 @@ def load_file(_data_file):
         raise ValueError('Failed to load data. Data file path is None.')
 
     return _data
-
-
-# FIXME save_file 함수 print, error 함수 변경필요 
+ 
 def save_file(_data, _data_file):
-    """ Description
-        -----------
-            - 데이터를 파일로 저장한다.
-        Parameters
-        -----------
-            - data : 파일로 저장할 데이터 (dict) : dataframe 등 key 포함 
+    """ save data into file.
+        extension supported: (pkl, json, params, log)
 
-            - data_file (str) : 저장할 데이터의 파일이름 (경로 포함)
-                                (확장자 지원 : csv, pkl, json, params, log)
-            - option
-                - _print (bool) : 데이터 저장여부 출력
-        Example
-        -----------
-            - save_file(data, data_file)
+    Args:
+        data    (dict): data tobe saved
+        data_file (str): data file saved path
+                         
+    Returns: -
+
     """
-
     if _data_file != None and not (isinstance(_data, str) and _data == 'none') and len(_data) > 0:
         try:
             check_path(_data_file)
@@ -76,7 +61,7 @@ def save_file(_data, _data_file):
                     _data_file.lower().endswith('.params') or \
                     _data_file.lower().endswith('.log'):
                 with open(_data_file, "w") as f:
-                    # ensure_ascii=False : 한글 지원
+                    ## ensure_ascii=False: suport Korean
                     json.dump(_data, f, indent=4, ensure_ascii=False)
             else:
                 raise TypeError('No Support file format (support : pkl, json, params, log)')
@@ -87,92 +72,91 @@ def save_file(_data, _data_file):
     else:
         pass
 
-
 def check_path(_filename, remake=False):
-    """ Description
-        -----------
-            Check the directory of file
+    """ Check file existence in the directory 
 
-        Parameters
-        -----------
-            _filename (str) : the file name with full directory 
-            options
-                remake (bool) : Create the path if not path
+    Args:
+        _filename (str) : the file name with full directory 
+        remake (bool) : Create the path if not path
+                         
+    Returns: 
+        Bool
 
-        Return
-        -----------
-            bool
-
-        Example
-        -----------
-            check_path('/home/user/work/filename.csv', remake=True)
     """
- 
-    # 접근 폴더가 없다면
+    ## if the directory not exist
     if not os.path.exists(os.path.dirname(_filename)):
         os.makedirs(os.path.dirname(_filename))
         return False
     else:
-        # 폴더가 있다면 삭제하고, 다시 만든다.
+        ## recreate directory 
         if remake == True:
             shutil.rmtree(os.path.dirname(_filename))
             os.makedirs(os.path.dirname(_filename))
         return True
  
-
 def _convert_variable_type(variable, target_type):
-        if not isinstance(target_type, str) or target_type.lower() not in ["str", "int", "float", "list", "bool"]:
-            raise ValueError("Invalid target_type. Allowed values are 'str', 'int', 'float', and 'list'.")
+    """ convert variable's type into target type 
 
-        if target_type.lower() == "str" and not isinstance(variable, str):
-            return str(variable)
-        elif target_type.lower() == "int" and not isinstance(variable, int):
-            return int(variable)
-        elif target_type.lower() == "float" and not isinstance(variable, float):
-            return float(variable)
-        elif target_type.lower() == "list" and not isinstance(variable, list):
-            return [variable]
-        elif target_type.lower() == "bool" and not isinstance(variable, bool):
-            if variable == "false" or variable == "False":
-                return False
-            else:
-                return True
+    Args:
+        variable    (*): variable tobe type-converted
+        target_type (*): target type
+                         
+    Returns: 
+        type converted variable
+
+    """
+    if not isinstance(target_type, str) or target_type.lower() not in ["str", "int", "float", "list", "bool"]:
+        raise ValueError("Invalid target_type. Allowed values are 'str', 'int', 'float', and 'list'.")
+    if target_type.lower() == "str" and not isinstance(variable, str):
+        return str(variable)
+    elif target_type.lower() == "int" and not isinstance(variable, int):
+        return int(variable)
+    elif target_type.lower() == "float" and not isinstance(variable, float):
+        return float(variable)
+    elif target_type.lower() == "list" and not isinstance(variable, list):
+        return [variable]
+    elif target_type.lower() == "bool" and not isinstance(variable, bool):
+        if variable == "false" or variable == "False":
+            return False
         else:
-            return variable
+            return True
+    else:
+        return variable
             
 def _extract_partial_data(_asset_data, _partial_load):             
-    """ Description
-        -----------
-            - load_data 시 사용자에게 반환되는 data dict의 key 중 _partial_load를 담고 있는 것만 추려서 data dict를 반환합니다. 
-            
-        Parameters
-        -----------
-            - _asset_data (dict)    :  data dict
-            - _partial_load (str)   : {HOME}/input/{pipline}/ 하위에 존재하는 경로 
-            
-        Return
-        -----------
-            - _asset_data           : 부분적으로 추려진 asset data 
-            
-        Example
-        -----------
-            - asset_data = _extract_partial_data(_asset_data, _partial_load)
+    """ During the load_data process, return a data dictionary to the user 
+        which contains only the keys that include {_partial_load}.
+
+    Args:
+        _asset_data (dict)    :  data dict
+        _partial_load (str)   : sub-path under the {HOME}/input/{pipline}/ 
+                         
+    Returns: 
+        partial k,v extract from {_asset_data}
+
     """
-    # 부분적으로 추릴 key를 list화 
+    ## partial keys to list 
     partial_key_list = [] 
     for k in _asset_data.keys(): 
         if _partial_load in k:  
             partial_key_list.append(k)
-    # partial k,v extract from asset_data
+    # partial k,v extract from {_asset_data}
     return dict(filter(lambda item: item[0] in partial_key_list, _asset_data.items()))
 
 def display_resource(step, msg):
-    '''
-    - msg: printed message (cpu usage)
-    ''' 
+    """ format conversion for displaying resource (memory, cpu) 
+
+    Args:
+        msg: message tobe printed
+                         
+    Returns: 
+        partial k,v extract from {_asset_data}
+
+    """
     assert type(msg) == str
-    # asset.py - decorator_run 에 mem, cpu decorator 붙어있는 점 활용
-    msg = "".join(["\033[93m", #bright yellow                                     
+    ## (Note) mem, cpu decorator in asset.py - decorator_run 
+    ## color: bright yellow 
+    msg = "".join(["\033[93m",                                     
     f"\n----------------------------------------------------------- Finished displaying < MEMORY > usage ( asset: {step} )\n",
     f"\n {msg} \n",
     f"----------------------------------------------------------- Finished displaying < CPU > usage ( asset: {step} ) \n",
